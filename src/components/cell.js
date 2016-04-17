5017b65cd4a6f2b33215264363871d926b95a9e8
Nicolas Cuillery <nicolas.cuillery@gmail.com>
2016-04-17 21:38:01 +0200

    publish undo

--- src/components/cell.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export class Cell extends Component {
-  render() {
-    return (
-      <span className="cell">{''}</span>
-    );
-  }
-}
--- src/game/init.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-
-import {size} from './conf';
-import {createTile} from './tile';
-
-export function init() {
-  const dimension = _.range(size);
-  return dimension.map(() => dimension.map(() => createTile()));
-}
